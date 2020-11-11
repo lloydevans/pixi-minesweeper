cb9d8aa21410ba5f703f4c5361d68b0de62e013d
Lloyd Evans <contact@lloydevans.co.uk>
2020-11-11 19:09:24 +0000

    Update utils.ts

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -4,3 +4,18 @@
 export function preventContextMenu() {
 	window.addEventListener("contextmenu", (e) => e.preventDefault(), false);
 }
+
+/**
+ * Loop array elements in random order.
+ *
+ * @param target - Target array.
+ * @param cb - Callback function.
+ */
+export async function forEachRandom<T>(target: T[], cb: (el: T) => Promise<void>) {
+	let _target = [...target];
+	while (_target.length > 0) {
+		let idx = Math.floor(Math.random() * _target.length);
+		let el = _target.splice(idx, 1)[0];
+		await cb(el);
+	}
+}
