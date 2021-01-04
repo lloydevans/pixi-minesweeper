ba9b626e964ef48743ef0794f472becada144746
Lloyd Evans <contact@lloydevans.co.uk>
2021-01-04 21:29:25 +0100

    Update bm-text.ts

--- src/common/bm-text.ts
+++ src/common/bm-text.ts
@@ -27,7 +27,14 @@ export class BmText extends PIXI.BitmapText {
 
 	protected options: BmTextOptions;
 
-	// Anchor is number | Point?
+	/**
+	 * Anchor is number | Point?
+	 * This is a slightly awkward hangover from the JS feature of setting
+	 * this value as either a number or a point object for 'convenience'.
+	 * Here I'm using this temporary value, and should probably try and get
+	 * this changed in Pixi by making a PR. I doubt anyone is particularly attached
+	 * to being able to set this as a number.
+	 */
 	public readonly _anchor = new PIXI.ObservablePoint(function (this: BmText) {
 		(this.anchor as PIXI.Point).x = this._anchor.x;
 		(this.anchor as PIXI.Point).y = this._anchor.y;
