92856fa477c88dae6ad71e6965c4bcb00a40b199
Lloyd Evans <contact@lloydevans.co.uk>
2025-12-06 11:31:11 +0000

    Remove unused type

--- src/common/tween-props.ts
+++ src/common/tween-props.ts
@@ -5,8 +5,6 @@ export interface TweenOptions {
 	reversed?: boolean;
 	bounce?: boolean;
 	timeScale?: number;
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	pluginData?: any;
 	paused?: boolean;
 	position?: number;
 	override?: boolean;
